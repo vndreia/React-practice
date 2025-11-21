e4def07885206b1970a34b69e8126b14804fed35
Andrea <andreapalacios481@gmail.com>
2025-11-20 21:58:06 -0600

    add: headerLogo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,10 @@
+import headerLogo from "../assets/logo.pnggit";
+
 function Header() {
   return (
     <div>
       <h1>¡Hola, Vite!</h1>
+      <img src={headerLogo} alt="Imagen de la aplicación"></img>
     </div>
   );
 }
